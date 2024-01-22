e7f5bd808c8eef3a25517c2ea12cc1022f201efa
Karo8870 <cazacuchristian@gmail.com>
2024-01-22 21:00:24 +0200

    testing serverless functions

--- back/index.js
+++ back/index.js
@@ -13,16 +13,20 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-mongoose
-  .connect(
-    "mongodb+srv://escape:escape12345@escaperoom.ro7f9ed.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("✅ Connected to database"))
-  .catch((e) => console.log("❌ Failed to connect to database", e));
+// mongoose
+//   .connect(
+//     "mongodb+srv://escape:escape12345@escaperoom.ro7f9ed.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then(() => console.log("✅ Connected to database"))
+//   .catch((e) => console.log("❌ Failed to connect to database", e));
 
-app.use("/api/rooms", RoomsRouter);
-app.use("/api/sessions", SessionsRouter);
-app.use("/api/teams", TeamRouter);
+// app.use("/api/rooms", RoomsRouter);
+// app.use("/api/sessions", SessionsRouter);
+// app.use("/api/teams", TeamRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Felicitari" });
+});
 
 app.listen(5000, () => {
   console.log("✅ Server started on port:", 5000);
